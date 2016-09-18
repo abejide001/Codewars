9d14bc1938a5f0411cf7ec98f43f7d046540d9c5
Priyank Patel <pripatel93@gmail.com>
2016-09-18 16:34:06 +0100

    Completed First N Primes Kata
    - Learnt about memoization
    -- Save results in cache and use them later to increase function performance.

--- Javascript/first-n-prime-numbers-5.js
+++ Javascript/first-n-prime-numbers-5.js
@@ -1,19 +1,18 @@
 //https://www.codewars.com/kata/first-n-prime-numbers
 
-
 var array = [];
 class p {
 
 	first(n){
-		//var array = [];
+
 		var i = 1;
 		var count, divisorFound;
-
-
-
+		if (array.length){
+			i = array[array.length-1]
+		}
 	    while (array.length < n) {
 	    i++;
-        divisorFound = false;
+	    divisorFound = false;
 	        for (count = 2; count < i; count++) {
 	            if (i % count === 0) {
 	                divisorFound = true;
@@ -25,11 +24,7 @@ class p {
 	        }
 	    }
 
-
-
 	    return array;
 	}
 }
-
 var Primes = new p()
-Primes.first(6012)
